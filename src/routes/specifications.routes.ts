2efb8617f6461cf49538100f62907a8a695e744b
Laercio Silva <laercio.sl@hotmail.com>
2023-07-27 20:45:41 -0300

    UseCase listSpecifications

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,19 +1,16 @@
 import { Router } from "express";
 
-import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
 import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
+import { listSpecificationsController } from "../modules/cars/useCases/listSpecifications";
 
 const specificationsRoutes = Router();
-
-const specificationsRepository = new SpecificationsRepository();
 
 specificationsRoutes.post("/", (request, response) => {
   return createSpecificationController.handle(request, response);
 });
 
 specificationsRoutes.get("/", (request, response) => {
-  const specifications = specificationsRepository.list();
-  return response.status(200).json(specifications);
+  return listSpecificationsController.handle(request, response);
 });
 
 export { specificationsRoutes };
