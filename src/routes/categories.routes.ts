53a0126c96ee6b1de92ea44e0b06cfd67e1d0b01
Laercio Silva <laercio.sl@hotmail.com>
2023-07-27 11:11:18 -0300

    adding uuid

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,23 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { Router } from "express";
+import { v4 as uuidV4 } from "uuid";
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({
+  const category = {
+    id: uuidV4(),
     name,
     description,
-  });
+  };
+
+  console.log(category);
+
+  categories.push(category);
 
   return response.status(201).send();
 });
