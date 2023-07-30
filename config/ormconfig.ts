3df10597148297a8de0ab13ecb7d299d566063fe
Laercio Silva <laercio.sl@hotmail.com>
2023-07-30 17:41:42 -0300

    create migration categories

--- config/ormconfig.ts
+++ config/ormconfig.ts
@@ -6,13 +6,18 @@ dotenv.config();
 
 const connectDB = new DataSource({
   type: "postgres",
-  host: "172.20.0.2",
+  host: "172.19.0.2",
   port: 5432,
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
   migrations: ["./src/database/migrations/*.ts"],
   entities: ["./src/modules/**/entities/*.ts"],
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 connectDB
