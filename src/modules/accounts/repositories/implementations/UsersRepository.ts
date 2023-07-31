0ef41c7d0aecedbbd9ddde4a1990a0d07ac1bec5
Laercio Silva <laercio.sl@hotmail.com>
2023-07-31 16:30:53 -0300

    create user

---
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,33 @@
+import { Repository } from "typeorm";
+
+import connectDB from "../../../../database";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = connectDB.getRepository(User);
+  }
+  async create({
+    name,
+    username,
+    email,
+    password,
+    driver_license,
+  }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      name,
+      username,
+      email,
+      password,
+      driver_license,
+    });
+
+    await this.repository.save(user);
+  }
+}
+
+export { UsersRepository };
