430cbc6c7e7fcfb9a07711abd47c2752066d581a
Laercio Silva <laercio.sl@hotmail.com>
2023-09-19 08:26:45 -0300

    refactoring list rentals by user

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -61,6 +61,7 @@ class RentalsRepository implements IRentalsRepository {
   async findRentalsByUser(user_id: string): Promise<Rental[]> {
     const rentalsByUser = await this.repository.find({
       where: { user_id },
+      relations: ["car"],
     });
 
     return rentalsByUser;
